73944ef84585c8993983bea00ecdf62442427096
AIE\s171681 <nicholasmeyers3@gmail.com>
2017-03-27 11:52:39 +1030

    Fixed the mess from the merge.

--- VersionControlTest/VersionControlTest.cpp
+++ VersionControlTest/VersionControlTest.cpp
@@ -17,19 +17,11 @@ int main()
 	string name;
 	cout << "Enter a number: ";
 	cin >> number;
-<<<<<<< HEAD
 	cout << "Enter your name; ";
 	cin >> name;
 	UserDetails user = UserDetails(name, number);
 	cout << "Hello " + user.GetName() << endl;
 	cout << user.GetNumber() << " divided by " << DIVIDER << " is " << user.GetNumber() / DIVIDER << endl;
-=======
-	cin.ignore();
-	getline(cin,name);
-	UserDetails user = UserDetails(name, number);
-	cout << "Hello " + user.GetName()<< endl;
-	cout << user.GetNumber() / DIVIDER << endl;
->>>>>>> d868d047eb3a610f1e3ea04b8cdd915f167d21b4
 	cout << "New changes here\n";
 	system("pause");
     return 0;
