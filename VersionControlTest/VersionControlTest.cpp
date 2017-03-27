bde5ac2250393951e7490842ee4f133626c86aad
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-03-27 12:00:32 +1030

    Program now prints a skull

--- VersionControlTest/VersionControlTest.cpp
+++ VersionControlTest/VersionControlTest.cpp
@@ -17,12 +17,23 @@ int main()
 	string name;
 	cout << "Enter a number: ";
 	cin >> number;
-	cout << "Enter your name; ";
+	cout << "Enter your name: ";
 	cin >> name;
 	UserDetails user = UserDetails(name, number);
 	cout << "Hello " + user.GetName() << endl;
 	cout << user.GetNumber() << " divided by " << DIVIDER << " is " << user.GetNumber() / DIVIDER << endl;
 	cout << "New changes here\n";
+	cout << "\n";
+	cout << "      00000     \n";
+	cout << "    000000000   \n";
+	cout << "   00000000000  \n";
+	cout << "   00  000  00  \n";
+	cout << "   00  000  00  \n";
+	cout << "   00000000000  \n";
+	cout << "   0000   0000  \n";
+	cout << "    000000000   \n";
+	cout << "     0 0 0 0    \n";
+	cout << "     0 0 0 0    \n";
 	system("pause");
     return 0;
 }
